c8137db85ba5b0d195a8b7f504f669395c126c59
Francisco Rodriguez <168039749+franciscoluis86@users.noreply.github.com>
2024-07-08 20:20:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import base64
 import os
 from twilio.rest import Client
-
-def hello_pubsub(event, context):
+source_number='+19123016292'
+destination_numbers=['+14386869856','+14388881882','+14388881882']
+def twilio_pubsub(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
          event (dict): Event payload.
@@ -10,15 +11,15 @@ def hello_pubsub(event, context):
     """
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message)
-
-    account_sid = ''
-    auth_token = ''
+ 
+    account_sid = 'ACa36d78c7e911728a95147b6530c463a5'
+    auth_token = 'a1b37925a8308ec04700097daeb15331'
     client = Client(account_sid, auth_token)
-    
-    message = client.messages.create(
-                              body='Message from twillo pubsub : ' +pubsub_message,
-                              from_='+16206588348',
-                              to='+14388881882'
-                          )
-
+    for each_destination_number in destination_numbers:
+          message = client.messages.create(
+                                        body='Message from twillo Francisco pubsub : ' +pubsub_message,
+                                        from_=source_number,
+                                        to=each_destination_number
+                                   )
+ 
     print(message.sid)
